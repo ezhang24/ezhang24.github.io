f6470cb83a3a43cd491b39d661efd0458837c885
Emily <elzhang@caltech.edu>
2023-01-04 22:13:04 -0800

    initial motion

--- next-multi-page-website/pages/index.js
+++ next-multi-page-website/pages/index.js
@@ -13,35 +13,31 @@ const fadeInUp = {
     opacity: 1,
     transition: {
       duration: 0.6,
-      ease: easing,
     },
   },
 };
 
 export default function Home() {
   return (
-      <main className={styles.main}>
-        <div className={styles.name_container}>
-
-          <motion.div variants={fadeInUp}>
-            <h3 className={styles.heading} style={{ color: "#AA7DCE" }}>
-              Hi, my name is
-            </h3>
-          </motion.div>
-
+    <main className={styles.main}>
+      <div className={styles.name_container}>
+        <h3 className={styles.heading} style={{ color: "#AA7DCE" }}>
+          Hi, my name is
+        </h3>
+        <motion.div variants={fadeInUp} initial="initial" animate="animate" >
           <h1 className={styles.title}>Emily Zhang</h1>
-        </div>
-        <div className={styles.description_container}>
-          <p className={styles.description}>
-            I'm an avid software engineer with a passion for learning new things
-            and creating technology that makes the world a more accessible
-            place. Currently, I've been interested in web development, robotics,
-            and AI.
-          </p>
-        </div>
-        <a className={styles.button} href="/about">
-          Learn more about me &rarr;
-        </a>
-      </main>
+        </motion.div>
+      </div>
+      <div className={styles.description_container}>
+        <p className={styles.description}>
+          I'm an avid software engineer with a passion for learning new things
+          and creating technology that makes the world a more accessible place.
+          Currently, I've been interested in web development, robotics, and AI.
+        </p>
+      </div>
+      <a className={styles.button} href="/about">
+        Learn more about me &rarr;
+      </a>
+    </main>
   );
 }
