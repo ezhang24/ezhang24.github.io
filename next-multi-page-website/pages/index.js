296212d0703f48c5427ef7159abbc554d1704040
Emily <elzhang@caltech.edu>
2023-01-04 22:27:49 -0800

    home page fade in motion

--- next-multi-page-website/pages/index.js
+++ next-multi-page-website/pages/index.js
@@ -1,8 +1,6 @@
 import styles from "../styles/Home.module.css";
 import { motion } from "framer-motion";
 
-const easing = [0.6, -0.05, 0.01, 0.99];
-
 const fadeInUp = {
   initial: {
     y: 60,
@@ -13,31 +11,41 @@ const fadeInUp = {
     opacity: 1,
     transition: {
       duration: 0.6,
+      staggerChildren: 0.2,
     },
   },
 };
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <motion.main
+      variants={fadeInUp}
+      initial="initial"
+      animate="animate"
+      className={styles.main}
+    >
       <div className={styles.name_container}>
-        <h3 className={styles.heading} style={{ color: "#AA7DCE" }}>
-          Hi, my name is
-        </h3>
-        <motion.div variants={fadeInUp} initial="initial" animate="animate" >
+        <motion.div variants={fadeInUp}>
+          <h3 className={styles.heading} style={{ color: "#AA7DCE" }}>
+            Hi, my name is
+          </h3>
+        </motion.div>
+        <motion.div variants={fadeInUp}>
           <h1 className={styles.title}>Emily Zhang</h1>
         </motion.div>
       </div>
-      <div className={styles.description_container}>
+      <motion.div className={styles.description_container} variants={fadeInUp}>
         <p className={styles.description}>
           I'm an avid software engineer with a passion for learning new things
           and creating technology that makes the world a more accessible place.
           Currently, I've been interested in web development, robotics, and AI.
         </p>
-      </div>
-      <a className={styles.button} href="/about">
-        Learn more about me &rarr;
-      </a>
-    </main>
+      </motion.div>
+      <motion.div variants={fadeInUp}>
+        <a className={styles.button} href="/about">
+          Learn more about me &rarr;
+        </a>
+      </motion.div>
+    </motion.main>
   );
 }
