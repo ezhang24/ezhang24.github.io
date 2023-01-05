253822855646da376ab8b78d3312ce5b95a492e5
Emily <elzhang@caltech.edu>
2023-01-04 22:09:25 -0800

    install

--- next-multi-page-website/pages/index.js
+++ next-multi-page-website/pages/index.js
@@ -1,22 +1,47 @@
-import Head from "next/head";
-import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { motion } from "framer-motion";
+
+const easing = [0.6, -0.05, 0.01, 0.99];
+
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing,
+    },
+  },
+};
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.name_container}>
-        <h3 className={styles.heading}>Hi, my name is</h3>
-        <h1 className={styles.title}>Emily Zhang</h1>
-      </div>
-      <div className={styles.description_container}>
-        <p className={styles.description}>
-          I'm an avid software engineer with a passion for learning new things
-          and creating technology that makes the world a more accessible place.
-          Currently, I've been interested in web development, robotics, and AI.
-        </p>
-      </div>
-      <a className={styles.button} href='/about'>Learn more about me &rarr;</a>
-    </main>
+      <main className={styles.main}>
+        <div className={styles.name_container}>
+
+          <motion.div variants={fadeInUp}>
+            <h3 className={styles.heading} style={{ color: "#AA7DCE" }}>
+              Hi, my name is
+            </h3>
+          </motion.div>
+
+          <h1 className={styles.title}>Emily Zhang</h1>
+        </div>
+        <div className={styles.description_container}>
+          <p className={styles.description}>
+            I'm an avid software engineer with a passion for learning new things
+            and creating technology that makes the world a more accessible
+            place. Currently, I've been interested in web development, robotics,
+            and AI.
+          </p>
+        </div>
+        <a className={styles.button} href="/about">
+          Learn more about me &rarr;
+        </a>
+      </main>
   );
 }
