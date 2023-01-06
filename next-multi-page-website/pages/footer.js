1269d54e7ae114884f0e62218cc1d5d5e86f11a1
Emily <elzhang@caltech.edu>
2023-01-05 23:50:23 -0800

    update footer animation

--- next-multi-page-website/pages/footer.js
+++ next-multi-page-website/pages/footer.js
@@ -1,17 +1,40 @@
 import styles from "../styles/Footer.module.css";
+import { motion } from "framer-motion";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
+      <script
+        src="https://kit.fontawesome.com/ddfeb5ea78.js"
+        crossorigin="anonymous"
+      ></script>
       <ul className={styles.media_container}>
         <li className={styles.list}>
-          <a href="https://github.com/ezhang24"><img className={styles.icon} src="github.svg"></img></a>
+          <a href="https://github.com/ezhang24">
+            <motion.img
+              whileHover={{ scale: 1.2 }}
+              className={styles.icon}
+              src="github.svg"
+            ></motion.img>
+          </a>
         </li>
         <li className={styles.list}>
-          <a href="https://www.linkedin.com/in/emily-zhang-2024/"><img className={styles.icon} src="linkedin.svg"></img></a>
+          <a href="https://www.linkedin.com/in/emily-zhang-2024/">
+            <motion.img
+              whileHover={{ scale: 1.2 }}
+              className={styles.icon}
+              src="linkedin.svg"
+            ></motion.img>
+          </a>
         </li>
         <li className={styles.list}>
-          <a href="mailto: elzhang@caltech.edu"><img className={styles.icon} src="email.svg"></img></a>
+          <a href="mailto: elzhang@caltech.edu">
+            <motion.img
+              whileHover={{ scale: 1.2 }}
+              className={styles.icon}
+              src="email.svg"
+            ></motion.img>
+          </a>
         </li>
       </ul>
     </div>
--- next-multi-page-website/pages/navbar.js
+++ next-multi-page-website/pages/navbar.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import styles from "../styles/Navbar.module.css";
-import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
